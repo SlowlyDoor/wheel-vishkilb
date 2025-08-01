f9ef88b52e85d5117ad84ebd2dfd2a4064cb8c4d
Mark Orlov <internalhell@yandex.ru>
2025-08-01 20:24:03 +0300

    правки визуала

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
          || { expand(){}, ready(){}, sendData:console.log, showAlert:alert, close(){} };
 
   tg.expand();
-  tg.ready();               // ← добавили
-  console.log(tg, tg.initDataUnsafe.user.first_name, tg.initDataUnsafe.user.username);
+  tg.ready();
+  console.log(tg.sendData("test"));
   /* ---------- URL-параметры ---------- */
   const url      = new URL(location.href);
   let   balance  = parseInt(url.searchParams.get('bal')  || '0', 10);
